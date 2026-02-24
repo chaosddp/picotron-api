e0605d42f510a3874811dfb4b0aa959ba79b550a
Chaos Yu <chaos.you@gmail.com>
2026-02-24 12:35:55 +0800

    remove local in def

--- api/window.lua
+++ api/window.lua
@@ -31,7 +31,6 @@ function set_draw_target(ud) end
 --- @field observe_drag_offset? boolean -- indicates that when items are dropped, their .xo,.yo is used
 --- @field background_updates? boolean  -- allow _update() callbacks when parent window is not visible
 --- @field background_draws? boolean    -- allow _draw() callbacks when parent window is not visible
-local WindowAttribs = {}
 
 --- Create a window and/or set the window's attributes
 --- @param attribs? WindowAttribs attributes of the window
